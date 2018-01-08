df955300934d2783ac5796d3392eace5d3fcf75a
FriedrichEckert <33749573+FriedrichEckert@users.noreply.github.com>
2018-01-08 16:06:57 +0100

    o changes in LCARS panel of AccessController hardware driver

--- src/de/tucottbus/kt/csl/hardware/accesscontrol/AccessController.java
+++ src/de/tucottbus/kt/csl/hardware/accesscontrol/AccessController.java
@@ -20,6 +20,7 @@ public class AccessController extends AAtomicHardware implements Runnable
 {
 
   // -- Fields --
+  static int[] PresenceTable = new int[30];
   
   /**
    * The guard thread running the TCP/IP connection to the RasPi.
@@ -145,24 +146,127 @@ public class AccessController extends AAtomicHardware implements Runnable
   
   public class LcarsSubPanel extends ElementContributor implements Observer
   {
-    final EValue eTest;
+   
+    final EValue[] ePresField = new EValue[30];
+    final EValue[] eTimeField = new EValue[30];
+    final EValue[] eVoltField = new EValue[30];
+    
+    final int marg = 3; // element margin
+    final int hE = 46; // element height
+    final int wE = 280; // element width
+    final int wV = 180; // Voltage element width
+    final int wT = 140; // Time element width
+    final int sco = (1220/2); // second coloumn offset (x direction)
+    
+    final EValue[] eStatusField = new EValue[10];
+    final int wSE = (1220/10)-marg; // status element width
+    
+    
     
     public LcarsSubPanel(int x, int y)
     {
       super(x,y);
       
-      eTest = new EValue(null,0,0,400,48,LCARS.ES_STATIC|LCARS.ES_LABEL_E|LCARS.ES_RECT_RND_W,"PERSON 1 IS");
-      eTest.setBlinking(true);
+      for (int c = 0; c < 15; c++)
+      {
+        // assemble panel left side
+        ePresField[c] = new EValue(null,0,c*(hE + marg),wE,hE,LCARS.ES_STATIC|LCARS.ES_LABEL_E|LCARS.ES_RECT_RND_W,"PERSON "+(c+1)+" IS");
+        eTimeField[c] = new EValue(null,wE,c*(hE + marg),wT,hE,LCARS.ES_STATIC|LCARS.ES_LABEL_E,"T:");
+        eVoltField[c] = new EValue(null,(wE+wT),c*(hE + marg),wV,hE,LCARS.ES_STATIC|LCARS.ES_LABEL_E,"V:");
+        
+        ePresField[c].setValue("UNKNOWN");
+        eVoltField[c].setValue("0mV");
+        eTimeField[c].setValue("0min");
+        
+        add(ePresField[c]);
+        add(eVoltField[c]);
+        add(eTimeField[c]);
+      }
+      
+      for (int c = 15; c < 30; c++)
+      {
+        // assemble panel right side
+        ePresField[c] = new EValue(null,sco,(c-15)*(hE + marg),wE,hE,LCARS.ES_STATIC|LCARS.ES_LABEL_E,"PERSON "+(c+1)+" IS");
+        eTimeField[c] = new EValue(null,sco+wE,(c-15)*(hE + marg),wT,hE,LCARS.ES_STATIC|LCARS.ES_LABEL_E,"T:");
+        eVoltField[c] = new EValue(null,sco+(wE+wT),(c-15)*(hE + marg),wV,hE,LCARS.ES_STATIC|LCARS.ES_LABEL_E|LCARS.ES_RECT_RND_E,"V:");
+
+        ePresField[c].setValue("UNKNOWN");
+        eVoltField[c].setValue("0mV");
+        eTimeField[c].setValue("0min");
+        
+        add(ePresField[c]);
+        add(eVoltField[c]);
+        add(eTimeField[c]);
+      }
+
+      // receivers
+      /*
+      for (int c = 0; c < 10; c++)
+      {
+        eStatusField[c] = new EValue(null,c*(wSE+marg),15*(hE + marg),wSE,hE,LCARS.ES_STATIC|LCARS.ES_LABEL_E,"RX"+(c+1));
+        
+        add(eStatusField[c]);
+      }
+      eStatusField[0].setStyle(LCARS.ES_RECT_RND_W); // make first field round on left side
+      eStatusField[9].setStyle(LCARS.ES_RECT_RND_E); // make las field round on right side
+*/
+      AccessController.this.addObserver(this);
+
+    /*
+    // Test working
+    final EValue eTest;
+    final EValue eTest2;
+    
+    public LcarsSubPanel(int x, int y)
+    {
+      super(x,y);
+      System.out.println("X: "+x+"\nY: "+y);
+      
+      eTest = new EValue(null,0,0,(1220/2)-3,50,LCARS.ES_STATIC|LCARS.ES_LABEL_E|LCARS.ES_RECT_RND_W,"PERSON 1 IS");
+      eTest2 = new EValue(null,(1220/2),0,(1220/2)-3,50,LCARS.ES_STATIC|LCARS.ES_LABEL_E|LCARS.ES_RECT_RND_E,"PERSON 2 IS");
+//      eTest.setBlinking(true);
       add(eTest);
+      add(eTest2);
       
       AccessController.this.addObserver(this);
+    }
+  */
     }
 
     @Override
     public void update(Observable o, Object arg)
     {
       System.err.println("notified: "+arg);
-      eTest.setValue("PRESENT");
+      
+      for(int c = 0; c < 30; c++)
+      {
+        switch(PresenceTable[c])
+        {
+          case 0: ePresField[c].setValue("ABSENT"); {break;}
+          case 1: ePresField[c].setValue("NEARBY"); {break;}
+          case 2: ePresField[c].setValue("PRESENT"); {break;}
+          default: ePresField[c].setValue("UNKNOWN"); {break;}
+        }
+//        eTimeField[c].setValue((c*3)+" MIN");
+//        eVoltField[c].setValue((3000-c)+" mV");
+                
+      }
+      
+      /*
+       * differentiate between runtimes:
+       * if < 1h -> in minutes
+       * if < 1d -> in hours
+       * if > 1d -> in days
+       */
+      
+      for(int c = 0; c < 10; c++)
+      {
+        eStatusField[c].setValue("1");
+      }
+
+      
+//      eTest.setValue("PRESENT");
+//      eTest2.setValue("ABSENT");
     }
   }
   
@@ -211,9 +315,15 @@ public class AccessController extends AAtomicHardware implements Runnable
 //    
 //    System.out.println("END OF MAIN");
 
+    for (int c = 0; c < 30; c++)
+    {
+      PresenceTable[c] = c % 3;
+    }
+    
     // TODO: This is the final main method -->
     args = LCARS.setArg(args,"--panel=",AccessControllerPanel.class.getName());
     LCARS.main(args);
+    getInstance().dispose();
     // <--
   }
 
